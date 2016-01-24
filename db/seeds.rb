923f87d7e09a8aff4afe0d839aa7278d0e5cbb4c
leighsn <leighscherrer@gmail.com>
2016-01-24 14:59:36 -0500

    merged styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# 
 
-Product.create(name: "Red Velvet Cake", description: "This cake is amazing", price: 200, image_url: "http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2004/1/23/1/ss1d26_red_velvet_cake.jpg.rend.sni12col.landscape.jpeg")
-Product.create(name: "Chocolate Cake", description: "This cake is delicious", price: 250, image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwjjwuSIkbzKAhXFJx4KHey_ADUQjRwIBw&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FChocolate_cake&psig=AFQjCNFRvdjUYvEjinOBLBl5CuvDOZfWvg&ust=1453507910586789")
-Product.create(name: "Carrot Cake", description: "This cake is orange", price: 150, image_url: "http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2007/2/16/0/ry0401_carrotcake.jpg.rend.sni12col.landscape.jpeg")
+Product.create(name:"Confetti Cake", description:"Perfect for birthdays", price: 90, image_url:"http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2014/3/13/0/FNK_Confetti-Birthday-Cake-Slice_s4x3.jpg.rend.sni12col.landscape.jpeg")
+Product.create(name:"Sugar Cookie", description:"Sweet and chewy", price: 2, image_url:"http://cdn-image.realsimple.com/sites/default/files/styles/rs_main_image/public/image/images/1010/SugarCookie_300.jpg?itok=e3T3Hg5w")
+Product.create(name:"Linzer Cookie", description:"Pretty as a flower", price:5, image_url:"http://cdn-image.realsimple.com/sites/default/files/styles/rs_photo_gallery_vert/public/linzer_123.jpg?itok=zTf63LAU")
+Product.create(name:"Chocolate Chip", description:"Classic and fantastic", price: 3, image_url:"http://cdn-image.realsimple.com/sites/default/files/styles/rs_photo_gallery_vert/public/image/images/1010/chocchip-ictcrop_gal.jpg?itok=vR5TFhM6")
+Product.create(name:"Pecan Sandies", description:"Pecan goodness", price: 5, image_url:"http://cdn-image.realsimple.com/sites/default/files/styles/rs_main_image/public/image/images/0912/pecan-sandy_300.jpg?itok=HlAfc8_5")
+Product.create(name: "Meringue Cookie", description: "Egg white and sugar heaven", price: 2, image_url: "http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2003/11/13/1/ee2d12_meringue_cookies.jpg.rend.sni12col.landscape.jpeg")
+
+Product.create(name: "Peanut Butter Cookie", description: "Flourless and gluten-free", price: 3, image_url: "http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/5/31/1/VF0607H_flourless-peanut-butter-cookies_s4x3.jpg.rend.sni12col.landscape.jpeg")
+Product.create(name: "Jam Thumbprints", description: "Simple ingredients, available in strawberry and apricot", price: 2, image_url: "http://cdn-image.realsimple.com/sites/default/files/styles/rs_photo_gallery_vert/public/image/images/0912/sugar-jam-thumbprints-ictcrop_gal.jpg?itok=3c3Ru4be")
+Product.create(name: "Snickerdoodles", description: "The taste of these will take you back to childhood", price: 4, image_url: "http://cdn-image.realsimple.com/sites/default/files/styles/rs_main_image/public/image/images/0912/snickerdoodle_300.jpg?itok=B9kWB-kO")
